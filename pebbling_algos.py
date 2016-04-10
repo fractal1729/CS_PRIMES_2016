78be0abbd883736a35031c34cb1749b885cb8955
fractal1729 <vinjai.vale@gmail.com>
2016-04-09 20:44:47 -0400

    Fixed a couple of bugs in depth_first_pebble

--- pebbling_algos.py
+++ pebbling_algos.py
@@ -7,10 +7,10 @@ P = pebble.PebbleGraph(3) # change 3 to whatever r is supposed to be
 # B: parent adjacency matrix; v: vertex to be pebbled.
 # I'm not 100% sure what S is but it seems to be a set of all the vertices that call on v to be pebbled
 def depth_first_pebble(P, v, S):
-	if(is_source(v)):
+	if(P.is_source(v)):
 		P.pebble(v)
 	for u in P.get_parents(v):
-		if(!pebbled(u)):
+		if(not pebbled(u)):
 			depth_first_pebble(P, u, utils.union(S, P.get_parents(v)))
 	P.pebble(v)
 	P.remove_pebbles(utils.complement1(P.size(), S))
